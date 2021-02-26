8b22a65ffa4598dd7191f0827ceb2ff8d9da2f9a
KaDiian <kadiidavidson@gmail.com>
2021-02-26 01:33:09 -0500

    Fixed Login

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from .config import Config
-USERNAME = 'admin'
-PASSWORD = 'password123'
-SECRET_KEY = 'Sup3r$3cretkey'
-UPLOAD_FOLDER = './uploads'
+
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config.from_object(Config)
 from app import views
