cf325e5728470bc1fd0ab6ea8851b1e7ed11d075
KaDiian <kadiidavidson@gmail.com>
2021-02-26 03:07:25 -0500

    Displayed Images

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ This file creates your application.
 """
 import os
 from app import app
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from werkzeug.utils import secure_filename
 from .forms import UploadForm
 
@@ -51,21 +51,22 @@ def get_upload_images():
     root_dir = os.getcwd()
     for subdir, dirs, files in os.walk(root_dir + '/uploads'):
         for file in files:
-            lst.append (os.path.join(subdir, file))
+            lst.append (file)
     return lst
 
 
 @app.route('/uploads/<filename>')
 def get_image(filename):
     rootdir = os.getcwd()
-    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
+    return send_from_directory(os.path.join(rootdir,app.config['UPLOAD_FOLDER']), filename)
  
 
 @app.route('/files')
 def files():
     if not session.get('logged_in'):
         abort(401)
-    return render_template('files.html', images=get_upload_images())
+    pics = get_upload_images()
+    return render_template('files.html', images=pics)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
